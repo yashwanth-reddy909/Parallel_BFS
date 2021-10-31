13da6f4b593d9462eb87ee77c019a817c94f9cd8
yashwanth-reddy909 <84513021+yashwanth-reddy909@users.noreply.github.com>
2021-10-31 10:24:29 +0530

    Add files via upload

---
+++ Serial_implementation.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <chrono>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+
+	int n,m;
+	cin>>n>>m;
+
+	vector<vector<int> > adj(n, vector<int>());
+
+	int prev;
+	int *number_of_edges = (int*)malloc(n*sizeof(int));
+	cin>>prev;
+
+	for(int i = 0; i < n; i++) {
+		int temp;
+		cin>>temp;
+		number_of_edges[i] = temp-prev;
+		prev = temp;
+	}
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < number_of_edges[i]; j++) {
+			int temp;
+			cin>>temp;
+			fflush(stdout);
+			adj[i].push_back(temp);
+		}
+	}
+
+	queue<int> q;
+	int *d = (int*)malloc(n*sizeof(int));
+	for(int i = 0; i < n; i++) d[i] = 1e9;
+	bool *vis = (bool*)malloc(n*sizeof(bool));
+	for(int i = 0; i < n; i++) vis[i] = false;
+	q.push(0);
+	vis[0] = true;
+	d[0] = 0;
+
+	int depth = 0;
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	while(!q.empty()) {
+		int v = q.front();
+		q.pop();
+		for(auto u:adj[v]) {
+			if(!vis[u]) {
+				vis[u] = true;
+				q.push(u);
+				d[u] = d[v]+1;
+				if(d[u] > depth) {
+					depth = d[u];
+				}
+			}
+		}
+	}
+
+	cout<<"The depth of the given graph from node 0 is "<<depth<<endl;
+
+	std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+
+	cout<<"Time taken for serial BFS: "<<(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count())/1000<<"ms\n";
+
+
+
+
+
+	return 0;
+}
---
+++ Work_efficient.cu
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cstdlib>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define BLOCK_SIZE 1024
+#define CEIL(a, b) ((a-1)/b +1)
+
+__global__ void work_efficient_parallel_bfs(int *d, int *R, int *C, int n, int *Q0, int *Q1, int *depth) {
+
+	int id = threadIdx.x;
+
+	for(int i = id; i < n; i+=blockDim.x) {
+		d[i] = 1e9;
+	}
+
+	__shared__ int Q0_len, Q1_len, current_depth;
+
+	if(id == 0) {
+		d[id] = 0;
+		Q0[id] = 0;
+		Q0_len = 1;
+		Q1_len = 0;
+		current_depth = 0;
+	}	
+	
+	__syncthreads();
+
+	while(Q0_len) {
+		for(int i = id; i < Q0_len; i+=blockDim.x) {
+			int v = Q0[i];
+			for(int j = R[v]; j < R[v+1]; j++) {
+				int u = C[j];
+				if(atomicCAS(&d[u], int(1e9), d[v]+1) == int(1e9)) {
+					int t = atomicAdd(&Q1_len, 1);
+					Q1[t] = u;
+				}
+			}
+		}
+
+		__syncthreads();
+
+		for(int i = id; i < Q1_len; i+=blockDim.x) {
+			Q0[i] = Q1[i];
+		}
+
+		__syncthreads();
+
+		if(id == 0) {
+			Q0_len = Q1_len;
+			Q1_len = 0;
+			current_depth++;
+		}
+
+		__syncthreads();
+ 	}
+
+ 	if(id == 0)
+ 		*depth = current_depth -1;
+}
+
+
+int main(int argc, char *argv[]) {
+
+	int n,m,x,y;
+  n=5;
+  m=5;
+
+
+	int *h_R = (int*)malloc((n+1)*sizeof(int));
+	vector<int> *v = new vector<int>[n+1];
+
+
+  for (int i = 0; i < m; i++) {
+    do {
+      x = rand() % n;
+      y = rand() % n;
+    } while (x == y);
+
+    // Edge between x and y
+    v[x].push_back(y);
+    v[y].push_back(x);
+  }
+
+  // h_R
+  h_R[0]=0;
+  int cur = 0;
+  for(int i=0; i<n; i++) {
+    cur += v[i].size();
+		h_R[i+1]=cur;
+  }
+
+
+	int *h_C = (int*)malloc(h_R[n]*sizeof(int));
+
+	// h_C 
+	int j1=0;
+	  for(int i=0; i<n; i++)
+        for(int it : v[i]){
+						h_C[j1++]=it;
+				}
+   
+
+	int *d_R, *d_C, *d_d, *Q0, *Q1, *d_depth;
+
+	cudaMalloc((void**) &d_R, (n+1)*sizeof(int));
+	cudaMalloc((void**) &d_C, h_R[n]*sizeof(int));
+	cudaMalloc((void**) &d_d, n*sizeof(int));
+	cudaMalloc((void**) &Q0, n*sizeof(int));
+	cudaMalloc((void**) &Q1, n*sizeof(int));
+	cudaMalloc((void**) &d_depth, sizeof(int));
+
+	cudaMemcpy(d_R, h_R, (n+1)*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_C, h_C, h_R[n]*sizeof(int), cudaMemcpyHostToDevice);
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start);
+
+	work_efficient_parallel_bfs<<<1, BLOCK_SIZE>>>(d_d, d_R, d_C, n, Q0, Q1, d_depth);
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+	cout<<"Compute time in GPU: "<<milliseconds<<"ms"<<endl;
+
+	int *h_d = (int*) malloc(n*sizeof(int));
+	int *h_depth = (int*) malloc(sizeof(int));
+
+	cudaMemcpy(h_d, d_d, n*sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_depth, d_depth, sizeof(int), cudaMemcpyDeviceToHost);
+	cout<<"The depth of the given graph from node 0 is "<<(*h_depth)<<endl;
+
+
+	return 0;
+}
